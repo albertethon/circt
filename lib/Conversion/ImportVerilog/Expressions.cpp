ff359b76d782213a7e0994dfc95935fe841cd3b8
ShiZuoye <albertethon@163.com>
2023-09-06 20:37:55 +0800

    [ImportVerilog] add Expression draft
    add expression draft in slang, which include
    `Call, Select, Assignment, Operator, Misc, Literal Expression`
    these will be implement in future.

--- lib/Conversion/ImportVerilog/Expressions.cpp
+++ lib/Conversion/ImportVerilog/Expressions.cpp
@@ -14,6 +14,44 @@ struct ExprVisitor {
   ExprVisitor(Context &context, Location loc) : context(context), loc(loc), builder(context.rootBuilder) {}
   mlir::OpBuilder builder;
 
+  //----------------------------------------------------------------------------------
+  //Literal Expressions visit
+  //----------------------------------------------------------------------------------
+
+  //TODO: 
+  Value visit(const slang::ast::RealLiteral &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::TimeLiteral &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::UnbasedUnsizedIntegerLiteral &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::NullLiteral &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::UnboundedLiteral &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::StringLiteral &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //----------------------------------------------------------------------------------
+  //Misc Expressions visit
+  //----------------------------------------------------------------------------------
+
   //XXX:
   Value visit(const slang::ast::NamedValueExpression &expr,const slang::ast::VariableSymbol &var){
     llvm::StringRef sr(expr.getSymbolReference()->name);
@@ -24,6 +62,183 @@ struct ExprVisitor {
         << expr.getSymbolReference()->name << "'";
     return nullptr;
   }
+
+  //TODO: 
+  Value visit(const slang::ast::HierarchicalValueExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::DataTypeExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::TypeReferenceExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::ArbitrarySymbolExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::LValueReferenceExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::EmptyArgumentExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::DistExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::CopyClassExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::MinTypMaxExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::ClockingEventExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::AssertionInstanceExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::TaggedUnionExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //----------------------------------------------------------------------------------
+  //Operator Expressions
+  //----------------------------------------------------------------------------------
+
+  //TODO: 
+  Value visit(const slang::ast::UnaryExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::BinaryExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::ConditionalExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::InsideExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::ConcatenationExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::ReplicationExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::StreamingConcatenationExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::OpenRangeExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //----------------------------------------------------------------------------------
+  //Assignment Expressions
+  //----------------------------------------------------------------------------------
+
+  //TODO: 
+  Value visit(const slang::ast::AssignmentExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::ConversionExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::SimpleAssignmentPatternExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::StructuredAssignmentPatternExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::ReplicatedAssignmentPatternExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::NewArrayExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::NewClassExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::NewCovergroupExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //----------------------------------------------------------------------------------
+  //Select Expressions
+  //----------------------------------------------------------------------------------
+
+  //TODO: 
+  Value visit(const slang::ast::ElementSelectExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::RangeSelectExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //TODO: 
+  Value visit(const slang::ast::MemberAccessExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+
+  //----------------------------------------------------------------------------------
+  //Call Expressions
+  //----------------------------------------------------------------------------------
+
+  //TODO: 
+  Value visit(const slang::ast::CallExpression &expr,const slang::ast::VariableSymbol &var){
+    assert(0 && "TODO");
+  }
+  
 
  /// Emit an error for all other types.
   template<typename T, typename... Args>
