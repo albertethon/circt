480f429d8fc20a2670355f6ad3f0f4891511b5f0
ShiZuoye <albertethon@163.com>
2023-09-06 16:03:26 +0800

    [ImportVerilog] Add support for the moore::VariableDeclOp
    support the declaration in moore such as
    int a = 12

---
+++ lib/Conversion/ImportVerilog/Expressions.cpp
@@ -0,0 +1,49 @@
+
+#include "ImportVerilogInternals.h"
+#include "slang/ast/ASTVisitor.h"
+
+using namespace circt;
+using namespace ImportVerilog;
+
+
+namespace {
+struct ExprVisitor {
+  Context &context;
+  Location loc;
+  ExprVisitor(Context &context, Location loc) : context(context), loc(loc), builder(context.rootBuilder) {}
+  mlir::OpBuilder builder;
+  Value visit(const slang::ast::IntegerLiteral &expr,const slang::ast::VariableSymbol &var){
+    Type tmpType = context.convertType(*var.getDeclaredType());
+    uint32_t value = expr.getValue().as<uint32_t>().value();
+    return builder.create<moore::VariableDeclOp>(loc,
+                    moore::LValueType::get(tmpType),
+                    builder.getStringAttr(var.name),
+                    value);
+  }
+
+
+
+  
+
+ /// Emit an error for all other types.
+  template<typename T, typename... Args>
+  Value visit(T&& node, Args&&... args) {
+    mlir::emitError(loc, "unsupported expr: ");
+    return nullptr;
+  }
+  /// Emit an error for all other types.
+  template<typename T, typename... Args>
+  Value visitInvalid(T&& node, Args&&... args) {
+    mlir::emitError(loc, "unsupported expr: ");
+    return nullptr;
+  }
+};
+} // namespace
+
+void Context::convertExpr(const slang::ast::VariableSymbol &varAst, LocationAttr loc) {
+  auto *expr = varAst.getInitializer();
+  if (!loc)
+    loc = convertLocation(expr->sourceRange.start());
+  auto value = expr->visit(ExprVisitor(*this, loc),varAst);
+
+}
